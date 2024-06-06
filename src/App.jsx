e0ede24e45cbbf90c437b823d38a07d855b45695
MARLEY CLYFF BONIFACIO DA SILVA <marley_c_silva@estudante.sesisenai.org.br>
2024-06-06 12:29:14 -0300

    animação + css

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,42 @@
 import './App.css'
+import { useState, useEffect } from "react"
+import {motion} from 'framer-motion'
+import styled from "styled-components"
 import Pokedex from './assets/components/Pokedex'
 
+const Spinner = styled(motion.div)`
+width: 50px;
+height: 50px;
+border: 4px solid black;
+border-top: 4px solid red;
+border-radius: 50%;
+`
 
 function App() {
 
+  const [Carregando, setCarregando] = useState(true);
+
+  useEffect(() =>{
+    const timer = setTimeout(() =>{
+      setCarregando(false)
+    },2000)
+    return() => clearTimeout(timer)
+  }, [])
+
   return (
     <>
-      <Pokedex />
-    </>
+    <div className="center">
+      {Carregando ? (
+      <Spinner
+      animate={{rotate: 360}}
+      transition={{duration: 1, repeat: Infinity, ease: 'linear'}}
+      />
+      ):(
+        <Pokedex visivel={!Carregando}/>
+      )}
+    </div>
+     
+    </> 
   )
 }
 
--- src/assets/components/Pokedex.jsx
+++ src/assets/components/Pokedex.jsx
@@ -1,40 +1,53 @@
-import{ useState, useEffect } from "react"
+import { useState, useEffect } from "react"
+import { motion } from 'framer-motion'
 
-export default function Pokedex(){
-const [ id, setId ] = useState(1); //iniciando id com valor 1
-const [ pokemon, setPokemon ] = useState(null); //iniciando dado pokemon com valor nulo
+export default function Pokedex(visivel) {
+    const [id, setId] = useState(1); //iniciando id com valor 1
+    const [pokemon, setPokemon] = useState(null); //iniciando dado pokemon com valor nulo
 
-const fetchData = async () =>{
-    try{
-        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
-        const data = await response.json();
-        setPokemon(data);
-    } catch(error){
-        console.error("Erro: ",error);
+    const fetchData = async () => {
+        try {
+            const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+            const data = await response.json();
+            setPokemon(data);
+        } catch (error) {
+            console.error("Erro: ", error);
+        }
     }
-}
 
-useEffect(() => {
-    fetchData();
-}, [id]
-);
+    useEffect(() => {
+        fetchData();
+    }, [id]
+    );
 
-const nextPokemon = () => {
-    setId(id + 1)
-}
+    const nextPokemon = () => {
+        setId(id + 1)
+    }
 
-const backPokemon = () => {
-    setId(id - 1)
-}
+    const backPokemon = () => {
+        setId(id - 1)
+    }
 
-    return(
-        <div className="pokedex">
+    return (
+        <motion.div
+            initial={{ opacity: 0, y: 50 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, Y: -50 }}
+            transition={{ duration: 1 }}
+        >
+            {visivel && 
+            <div className="pokedex">
             <h1>Pokedex</h1>
             {pokemon && (
                 <div className="pokemon">
-                <p>{pokemon.name}</p>
-                <img  className="pokeimg" src={pokemon.sprites.other.showdown.front_default} alt={pokemon.name}/>{/* .sprites.other.home.front_default  */}
-                
+                    <p className="tiny5-regular">{pokemon.name}</p>
+                    <div className="fundopoke">
+                        <img className="pokeimg2" src="pokeball.png" alt="pokebola" />
+                        <div className="fundopoke2">
+                            <img className="pokeimg" src={pokemon.sprites.front_default} alt={pokemon.name} />{/* .sprites.other.home.front_default  */}
+                        </div>
+                    </div>
+
                     <div className="button">
                         <button onClick={backPokemon}>Anterior</button>
                         <button onClick={nextPokemon}>Próximo</button>
@@ -43,5 +56,8 @@ const backPokemon = () => {
             )
             }
         </div>
+            }
+        </motion.div>
+        
     )
 }
